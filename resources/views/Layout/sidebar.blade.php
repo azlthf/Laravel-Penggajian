dbbcea58c3f49487785e29efd604595f09cb5b10
azlthf <althafzikri@gmail.com>
2022-12-04 16:35:42 +0700

    first commit

---
+++ resources/views/Layout/sidebar.blade.php
@@ -0,0 +1,52 @@
+<nav id="sidebar">
+				<div class="p-4 pt-5">
+				<h3 class="text-center text-warning">PT GROW UP</h3>
+		  		<a href="#" class="img logo rounded-circle mb-5" style="background-image: url({{ asset('img/avatar.png') }});"></a>
+	        <ul class="list-unstyled components mb-5">
+	          <li class="active">
+	            <a href="/Home">Home</a>
+	            <!-- <ul class="collapse list-unstyled" id="homeSubmenu">
+                <li>
+                    <a href="#">Home 1</a>
+                </li>
+                <li>
+                    <a href="#">Home 2</a>
+                </li>
+                <li>
+                    <a href="#">Home 3</a>
+                </li>
+	            </ul> -->
+	          </li>
+	          <li>
+	              <a href="/jabatan/index">Jabatan</a>
+	          </li>
+	          <li>
+              <a href="/karyawan/index">Karyawan</a>
+              <!-- <ul class="collapse list-unstyled" id="pageSubmenu">
+                <li>
+                    <a href="#">Page 1</a>
+                </li>
+                <li>
+                    <a href="#">Page 2</a>
+                </li>
+                <li>
+                    <a href="#">Page 3</a>
+                </li>
+              </ul> -->
+	          </li>
+	          <li>
+              <a href="/Gaji/index">Gaji</a>
+	          </li>
+	          <li>
+              <a href="/Laporan">Laporan</a>
+	          </li>
+	        </ul>
+
+	        <div class="footer">
+	        	<p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+						  <!-- Copyright &copy;<script>document.write(new Date().getFullYear());</script> Zikri Althaf <i class="icon-heart" aria-hidden="true"></i> <a href="https://colorlib.com" target="_blank"></a> -->
+						  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+	        </div>
+
+	      </div>
+    	</nav>
